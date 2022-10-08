f7c7860cab9f6f47c77c5ca047702def3fc530d3
arekkusu6 <github.mail2116@gmail.com>
2022-10-08 03:33:57 -0400

    game thread start & game loop. rendering white rectangle as player for key handling next

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -3,7 +3,7 @@ package main;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
     // Screen Settings
     final int originalTileSize = 16; // 16x16 tile
     final int scale = 3; // 3x3 scale
@@ -14,9 +14,34 @@ public class GamePanel extends JPanel {
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
+    Thread gameThread;
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+    }
+
+    public void startGame() {
+        gameThread = new Thread(this);
+        gameThread.start(); // calls the run() method
+    }
+    @Override
+    public void run() {
+        while (gameThread.isAlive()){
+             update();
+             repaint();
+        }
+    }
+
+    public void update() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setColor(Color.white);
+        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.dispose(); // dispose graphics context to save memory (release sys resources)
     }
 }
